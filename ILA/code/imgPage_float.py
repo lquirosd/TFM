1d626b1e15772b249be30b45110503fd6e8e4ebd
lquirosd <lorenzoqd@gmail.com>
2016-08-03 13:07:21 +0200

    Addding Brute Force Search

--- ILA/code/imgPage_float.py
+++ ILA/code/imgPage_float.py
@@ -158,13 +158,89 @@ def getPCA(data, dims=2):
     # and return the re-scaled data, eigenvalues, and eigenvectors
     return np.dot(evecs.T, data.T).T
 
+def computeII(data):
+    """
+    Computes Integral Image as defined on 
+    Lewis, J.P. (1995). Fast template matching. Proc. Vision Interface
+    """
+    return data.cumsum(axis=0).cumsum(axis=1)
 
+def getIIsum(data, X):
+    """
+    Compute summed area as:
+       A=U          Bi=U[0],B[1]
+        +----------+
+        |          |
+        |          |
+        +----------+
+       C=B[0],U[1]  D=B
 
+    \sum = I(D) - I(A) + I(Bi) + I(C)
+    """
+    if (X[0:2] == X[2:4]):
+        return data[U]
+    else:
+        return data[X[0:2]] - data[X[2:4]] + data[X[0], X[2]] + data[X[3], X[1]]
 
+def getGMMlog(GMM, X):
+    return  GMM['Upper'].score(np.array([[X[0], X[1]]])) + \
+            GMM['Bottom'].score(np.array([[X[2],X[3]]]))
+
+class BBox(object):
+   def __init__(self, x1, y1, x2, y2):
+      '''
+      (x1, y1) is the upper left corner,
+      (x2, y2) is the lower right corner,
+      with (0, 0) being in the upper left corner.
+      '''
+      if x1 > x2: x1, x2 = x2, x1
+      if y1 > y2: y1, y2 = y2, y1
+      self.x1 = x1
+      self.y1 = y1
+      self.x2 = x2
+      self.y2 = y2
+      self.area = (x2 - x1) * (y2 - y1)
+   def taxicab_diagonal(self):
+      '''
+      Return the taxicab distance from (x1,y1) to (x2,y2)
+      '''
+      return self.x2 - self.x1 + self.y2 - self.y1
+   def overlaps(self, other):
+      '''
+      Return True iff self and other overlap.
+      '''
+      return not ((self.x1 > other.x2)
+                  or (self.x2 < other.x1)
+                  or (self.y1 > other.y2)
+                  or (self.y2 < other.y1))
+   def __eq__(self, other):
+      return (self.x1 == other.x1
+               and self.y1 == other.y1
+               and self.x2 == other.x2
+               and self.y2 == other.y2)
+
+def find_paws(data, smooth_radius = 5, threshold = 0.0001):
+   """Detects and isolates contiguous regions in the input array"""
+   # Blur the input data a bit so the paws have a continous footprint 
+   data = ndi.uniform_filter(data, smooth_radius)
+   # Threshold the blurred data (this needs to be a bit > 0 due to the blur)
+   thresh = data < threshold
+   # Fill any interior holes in the paws to get cleaner regions...
+   filled = ndi.morphology.binary_fill_holes(thresh)
+   # Label each contiguous paw
+   coded_paws, num_paws = ndi.label(filled)
+   # Isolate the extent of each paw
+   # find_objects returns a list of 2-tuples: (slice(...), slice(...))
+   # which represents a rectangular box around the object
+   data_slices = ndi.find_objects(coded_paws)
+   return data_slices
+
+def slice_to_bbox(slices):
+   for s in slices:
+      dy, dx = s[:2]
+      yield BBox(dx.start, dy.start, dx.stop+1, dy.stop+1)
 
 def test_module():
-   
-
     #imgP = "/Users/lquirosd/Documents/MsC_MIARFID/MIARFID/TFM/ILA/DataCorpus/test/Mss_003357_0958_pag-825[857].jpg"
     init = time.clock()
     imgP = "/home/lorenzoqd/TFM/ILA/DataCorpus/Plantas-1/Mss_003357_0958_pag-825[857].jpg"
---
+++ ILA/code/predictLayoutBruteForce.py
@@ -0,0 +1,136 @@
+import numpy as np
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+from scipy import misc
+from scipy import optimize
+import scipy.ndimage as ndi
+import imgPage_float as imgPage
+import sys, argparse #--- To handle console arguments 
+import matplotlib.patches as patches
+#import bbox
+import time
+try:
+    import cPickle as pickle
+except:
+    import pickle #--- To handle data export
+import subprocess as shell
+
+def minFun(x, GMM, p0, p1):
+    sumP0 = imgPage.getIIsum(p0, x)
+    sumP1 = imgPage.getIIsum(p1, x)
+    valGMM = imgPage.getGMMlog(GMM, x)
+    return -sumP0 - sumP1 - valGMM
+
+def findBboxBF(GMM, P0, P1):
+    #--- compute II
+    p0II = imgPAge.computeII(P0)
+    p1II = imgPAge.computeII(P1)
+    r, c = P0.shape
+    #--- using r/2 in order to reduce grid size, but Bootm point cant be reduced
+    #--- in general case, since bbox could be pretty small 
+    #--- Use small set for testing
+    rranges(slice(5,r/2,20), slice(5,c/2,20), slice(r/2,r,20), slice(c/2,c,20) )
+    params = (GMM, p0II, p1II)
+    resBrute = optimize.brute(minFun, rranges, args=params, full_output=True, finish=optimize.fmin)
+
+
+def main():
+    #--- processing arguments 
+    parser = argparse.ArgumentParser(description='Layout Analysis')
+    parser.add_argument('-imgData', action="store", help="Pointer to images Data pickle file")
+    parser.add_argument('-t', '--testDir', action="store", help="Pointer to CRFs model file")
+    parser.add_argument('-s', '--statistics', action="store_true", help="Print some statistics about script execution")
+    parser.add_argument('--debug', action="store_true", help="Run script on Debugging mode")
+    args = parser.parse_args()
+    if (args.debug): print args 
+    if(args.statistics): init = time.clock() 
+
+    #--- Read imgData 
+    fh = open(args.imgData, 'r')
+    imgData = pickle.load(fh)
+    fh.close()
+    #--- use only first image in order to test code 
+    for img in imgData[0]:
+        #--- read img
+        if(args.statistics): Decinit = time.clock() 
+        print "Working on {}...".format(img.name)
+        img.readImage(zoom=img.zoom)
+        #--- window and granularity should be extracted from model, but isnt on test model yet
+        img.window = 32
+        img.granularity = 3
+        #--- Results Format: Label\smarginalProb
+        #--- Since we are working on 2 class problem, P(0) = 1- P(1) 
+        rData = np.loadtxt(args.testDir + '/' + img.name + '.results')
+        labels = rData[:,0]
+        P0 = results[:,1].copy()
+        P1 = results[:,1].copy()
+        P0[results[:,0]==1] = 1 - P0[results[:,0]==1]
+        P1[results[:,0]==1] = 1 - P1[results[:,0]==1]
+        rows = np.arange(0,img.imgShape[0],img.granularity)
+        rows = rows[np.where((rows>(img.window/2)) & (rows<=(img.imgShape[0]-(img.window/2))))]
+        colums = np.arange(0,img.imgShape[1],img.granularity)
+        colums = colums[np.where((colums>(img.window/2)) & (colums<=(img.imgShape[1]-(img.window/2))))]
+        zm = np.zeros(img.imgShape)
+        z0 = np.zeros(img.imgShape)
+        z1 = np.zeros(img.imgShape)
+        labels = labels.reshape(rows.size, colums.size)
+        P0 = P0.reshape(rows.size, colums.size)
+        P1 = P1.reshape(rows.size, colums.size)
+        if(args.statistics): Rinit = time.clock() 
+        for r, row in enumerate(rows):
+            for c, col in enumerate(colums):
+                winIndex = np.ix_(np.arange(row-(img.granularity/2),row+(img.granularity/2)+1,1,dtype=int),
+                        np.arange(col-(img.granularity/2),col+(img.granularity/2)+1,1,dtype=int))
+                zm[winIndex] = labels[r,c]
+                z0[winIndex] = P0[r,c]
+                z1[ winIndex] = P1[r,c]
+        zm[zm==0] = 255
+        zm[zm==1] = 0
+        if (args.statistics): print 'Expand: {0:.5f} seconds'.format(time.clock() - Rinit)
+        if (args.statistics): print 'Decoding: {0:.5f} seconds'.format(time.clock() - Decinit)
+        #--- Find Main Paragraph using PAWS
+        if(args.statistics): PAWSinit = time.clock() 
+        data_slices = imgPage.find_paws(zm, smooth_radius = 20, threshold = 22)
+        bboxes = imgPage.slice_to_bbox(data_slices)
+        maxArea = 0
+        fig, ax = plt.subplots( nrows=1, ncols=1 )
+        ax.imshow(img.img, cmap='gray')
+        #--- get real bbox
+        Up = img.getUpperPoints()
+        Bp = img.getBottomPoints()
+        rPatch = patches.Rectangle((Up[0],Up[1]), Bp[0]-Up[0], Bp[1]-Up[1],
+                fc = 'none', ec = 'blue')
+        ax.add_patch(rPatch)
+        #--- Adding PAWS result
+        ax.imshow(zm, alpha=.4, cmap='viridis')
+        for box in bboxes:
+            xwidth = box.x2 - box.x1
+            ywidth = box.y2 - box.y1
+            area = xwidth * ywidth
+            if (area > maxArea):
+                xW = xwidth
+                yW = ywidth
+                x1 = box.x1
+                y1 = box.y1
+                maxArea = area
+        p = patches.Rectangle((x1, y1), xW, yW,
+                                fc = 'none', ec = 'red')
+        ax.add_patch(p)
+        if (args.statistics): print 'PAWS Time: {0:.5f} seconds'.format(time.clock() - PAWSinit)
+        #--- Find Main Paragraph using Brute Force 
+        if(args.statistics): BFinit = time.clock() 
+        bruteResults = findBboxBF(GMMmodel, z0, z1) 
+        bruteBbox = bruteResults[0]
+        bruteLogScore = bruteResults[1]
+        if (args.statistics): print 'Brute Force: {0:.5f} seconds'.format(time.clock() - BFinit)
+        #--- Save Results
+        fig.savefig(args.testDir + '/' + img.name + '.png')
+        plt.close(fig)
+        if (args.statistics): print 'Total Time: {0:.5f} seconds'.format(time.clock() - init)
+
+    print "Done..."
+
+
+if __name__ == '__main__':
+    main()
